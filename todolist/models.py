29f44171e4cab477cda4a19d4f3c19e8be389f33
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-07-26 14:12:24 +0200

    added todo model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,17 @@
+import datetime
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class TodoModel(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    created_at = models.DateField(default=datetime.date.today)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    is_done = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f'({self.id}) {self.title}'
