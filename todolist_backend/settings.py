ccbfe812017210d11449099c443d84a0618114dc
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-07-26 13:42:40 +0200

    login view

--- todolist_backend/settings.py
+++ todolist_backend/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-*=h#jg6a9!(^!bi&_ew(%ju@fu!q_zzj$jb!zxmj)b(wl*uxav
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+]
 
 
 # Application definition
@@ -38,10 +44,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'todolist'
+    'rest_framework.authtoken',
+    'todolist',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +132,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django Rest Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
